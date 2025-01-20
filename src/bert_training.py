69bc3c60c9507114d6dfe4595e088b01441e95e6
Evan Johnson <ecrjmegamanx@gmail.com>
2025-01-20 17:33:20 -0500

    finished bert training script

--- src/bert_training.py
+++ src/bert_training.py
@@ -1,18 +1,26 @@
-import torch.nn as nn
-from transformers import BertForSequenceClassification, BertModel, GPT2Model, GPT2Tokenizer, Trainer, TrainingArguments
+from transformers import BertForSequenceClassification, GPT2Model, GPT2Tokenizer, Trainer, TrainingArguments
 from datasets import load_dataset
 
-class bert(BertForSequenceClassification):
-    def
+class bert_encoder(BertForSequenceClassification):
+    def forward(self, input_embeds, **kwargs):
+        return super().forward(inputs_embeds=input_embeds, **kwargs)
 
+bert = bert_encoder.from_pretrained('bert-base-uncased', num_labels=2)
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 gpt = GPT2Model.from_pretrained('gpt2')
+gpt_embedder = gpt.get_input_embeddings()
 
-def tokenizer_fn(example):
-    return tokenizer(example['text'], padding=True, truncation=True)
-
+def embedder_fn(example):
+    tokens = tokenizer(example['text'], padding=True, truncation=True, return_tensors='pt')
+    embeddings = gpt_embedder(tokens['input_ids'])
+    return {
+        'input_embeds': embeddings,
+        'labels': example['label'],
+        'attention_mask': tokens['attention_mask']
+    }
+    
 dataset = load_dataset('glue', 'sst2')
-tokenized_datasets = dataset.map(tokenizer_fn, batched=True)
+tokenized_datasets = dataset.map(embedder_fn, batched=True, remove_columns=['text', 'input_ids'])
 
 train_dataset = tokenized_datasets['train']
 eval_dataset = tokenized_datasets['validation']
