5173f25e00f16817c49fd8d2efc324f5fe689b29
Evan Johnson <ecrjmegamanx@gmail.com>
2025-01-21 21:14:57 -0500

    starting training

--- src/gpt_conservative_training.py.py
+++ src/gpt_conservative_training.py.py
@@ -1,4 +1,10 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, Trainer, TrainingArguments
+from datasets import load_dataset
 
 gpt = GPT2LMHeadModel.from_pretrained('gpt2')
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
+dataset = load_dataset('cajcodes/political-bias')
+
+conservative_dataset = dataset['train'].filter(lambda x: x['label'] == 0)
+liberal_dataset = dataset['train'].filter(lambda x: x['label'] == 4)
